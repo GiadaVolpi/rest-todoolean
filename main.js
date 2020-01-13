ac526eecedc82f66465c94a92c2d8d18311ed16d
Gi@da <giadavolpi8@gmail.com>
2020-01-13 17:28:05 +0100

    inserimento nuovi todo

--- main.js
+++ main.js
@@ -3,28 +3,69 @@ $(document).ready (function () {
     var template_html = $("#template").html();
     var template_function = Handlebars.compile(template_html);
 
-    $.ajax ({
-        "url": "http://157.230.17.132:3024/todos/",
-        "method": "GET",
-        "success": function (data) {
-            var todos = data;
-            for (var i = 0; i < todos.length; i++) {
-                var todoText = todos[i].text;
-                console.log(todoText);
+    stampaLista();
 
-                var context = {
-                    todoText: todoText
-                }
+    //intercetto il click sul pulsante inserisci
+    $("#button-new-todo").click(function(){
+        //leggo il testo inserito dall'utente
+        var nuovoTodo = $("#input-new-todo").val().trim();
 
-                var html_finale = template_function(context);
-                $("#todo-list").append(html_finale);
-            }
-        },
-        "error": function() {
-            alert ("Error");
+        if (nuovoTodo.length > 0) {
+            //resetto l'input
+            $("#input-new-todo").val("");
+
+            creazioneTodo(nuovoTodo);
         }
-    });
 
+    })
+
+
+
+    /* ------------------------ funzioni ------------------------ */
+
+    function stampaLista () {
+        // chiamata ajax in get per leggere le API
+        $.ajax ({
+            "url": "http://157.230.17.132:3024/todos/",
+            "method": "GET",
+            "success": function (data) {
+                //resetto la lesta dei todo
+                $ ("#todo-list").empty();
+
+                var todos = data;
+                for (var i = 0; i < todos.length; i++) {
+                    var todoText = todos[i].text;
+                    var todo_id = todos[i].id
+                    var context = {
+                        todoId: todo_id,
+                        todoText: todoText
+                    }
+                    var html_finale = template_function(context);
+                    $("#todo-list").append(html_finale);
+                }
+            },
+            "error": function() {
+                alert ("Error");
+            }
+        });
+    }
+
+    function creazioneTodo (newText) {
+        //chiamata ajax in get per leggere le API
+        $.ajax ({
+            "url": "http://157.230.17.132:3024/todos/",
+            "method": "POST",
+            "data": {
+                "text": newText
+            },
+            "success": function (data) {
+                stampaLista();
+            },
+            "error": function() {
+                alert ("Error");
+            }
+        });
+    }
 
 
 })
